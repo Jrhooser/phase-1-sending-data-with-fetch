3fcd5d1b557de8ce07cca57c3e0150fda619c05f
JacobHooser <jacob.hooser@yahoo.com>
2022-08-01 15:47:07 -0500

    commit tree b1f52475dfdfe7e96d28c886a46e66f5243728bf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const pleaseLetThisWork = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify ({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    };
+    return fetch("http://localhost:3000/users", pleaseLetThisWork)
+    .then(function (resp) {
+        return resp.json()
+    })
+    .then(function (object) {
+        document.body.innerHTML = object['id']
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
